8e83aff1659cb83d993d3f213adc59e270033c8d
Bjørn Lau Jørgensen <bjornlau@Bjrns-MacBook-Pro.local>
2019-11-19 18:33:43 +0100

    Added logic to prevent the same questions from being used

--- JaywayQuiz/ViewController.swift
+++ JaywayQuiz/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var currentCorrectAnswer: String?
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -18,7 +18,51 @@ class ViewController: UIViewController {
     @IBOutlet weak var answerLabel2: UIButton!
     @IBOutlet weak var answerLabel3: UIButton!
     
-    func setupQuestions(questionObjects: [question]) {
+    
+    func prepareQuestionsForGame(questionObjects: [question], numberOfQuestions: Int) {
+        
+        let shuffledData = questionObjects.shuffled()
+        
+        let unusedQuestions = shuffledData.filter(){$0.isQuestionUsed == .notUsed}
+        let usedQuestions = shuffledData.filter(){$0.isQuestionUsed == .used}
+        
+        print("UNUSED QUESTIONS: \(unusedQuestions.count)")
+        print("USED QUESTIONS: \(usedQuestions.count)")
+        
+        var gameReadyQuestions = [question]()
+    
+        if unusedQuestions.count >= numberOfQuestions {
+            gameReadyQuestions = Array(unusedQuestions.prefix(numberOfQuestions))
+        } else {
+            
+            let neededQuestions = 10 - unusedQuestions.count
+            
+            for i in 0..<10 - neededQuestions {
+                gameReadyQuestions.append(unusedQuestions[i])
+                print("Appending: \(unusedQuestions[i].questionString)")
+            }
+            
+            print("NEEDED: \(neededQuestions)")
+            for i in 0..<neededQuestions {
+                gameReadyQuestions.append(usedQuestions[i])
+                print("Appending: \(usedQuestions[i].questionString)")
+            }
+            
+        }
+        
+        //  MARK THE QUESTIONS THAT HAVE BEEN USED
+        for elements in gameReadyQuestions {
+            if let index = questionArray.firstIndex(where: { $0.questionID == elements.questionID }) {
+                questionArray[index].isQuestionUsed = .used
+            }
+        }
+        
+    }
+    
+    
+    
+    
+    func presentQuestion(questionObjects: [question]) {
         let randomQuestion = questionObjects.randomElement()
         
         answerLabel0.setTitle(randomQuestion?.answers[0], for: .normal)
@@ -29,26 +73,35 @@ class ViewController: UIViewController {
         currentCorrectAnswer = randomQuestion?.correctAnswer
     }
     
-    func checkAnswer(answer: String) {
+    func checkAnswer(answer: String) -> Bool {
         if answer == currentCorrectAnswer {
             print("Correct answer!")
+            return true
         } else {
             print("Wrong answer!")
+            return false
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupQuestions(questionObjects: questionArray)
+        presentQuestion(questionObjects: questionArray)
+        prepareQuestionsForGame(questionObjects: questionArray, numberOfQuestions: 10)
     }
     
     
     @IBAction func answerButton(_ sender: UIButton) {
         if let buttonTitle = sender.title(for: .normal) {
-            checkAnswer(answer: buttonTitle)
+            if checkAnswer(answer: buttonTitle) == true {
+                sender.tintColor = UIColor.green
+            } else {
+                sender.tintColor = UIColor.red
+            }
         }
-        
     }
     
+    @IBAction func playAgain(_ sender: Any) {
+        prepareQuestionsForGame(questionObjects: questionArray, numberOfQuestions: 10)
+    }
 }
 
--- JaywayQuiz/data.swift
+++ JaywayQuiz/data.swift
@@ -9,12 +9,24 @@
 import Foundation
 import UIKit
 
-class question {
+
+
+var questionArray = [question(questionID: 1, questionString: "A - Question?", questionImage: nil, answers: ["A - Answer1", "A - Answer2", "A - Answer3", "A - Answer4"], correctAnswer: "A - Answer3"), question(questionID: 2, questionString: "B - QUESTION?", questionImage: nil, answers: ["B - ANSWER1", "B - ANSWER2", "B - ANSWER3", "B - ANSWER4"], correctAnswer: "B - ANSWER1"), question(questionID: 3, questionString: "C - QUESTION?", questionImage: nil, answers: ["C - ANSWER1", "C - ANSWER2", "C - ANSWER3", "C - ANSWER4"], correctAnswer: "C - ANSWER1"), question(questionID: 4, questionString: "D - QUESTION?", questionImage: nil, answers: ["D - ANSWER1", "D - ANSWER2", "D - ANSWER3", "D - ANSWER4"], correctAnswer: "D - ANSWER1"), question(questionID: 5, questionString: "E - QUESTION?", questionImage: nil, answers: ["E - ANSWER1", "E - ANSWER2", "E - ANSWER3", "E - ANSWER4"], correctAnswer: "E - ANSWER1"), question(questionID: 6, questionString: "F - QUESTION?", questionImage: nil, answers: ["F - ANSWER1", "F - ANSWER2", "F - ANSWER3", "F - ANSWER4"], correctAnswer: "F - ANSWER1"), question(questionID: 7, questionString: "G - QUESTION?", questionImage: nil, answers: ["G - ANSWER1", "G - ANSWER2", "G - ANSWER3", "G - ANSWER4"], correctAnswer: "G - ANSWER1"), question(questionID: 8, questionString: "H - QUESTION?", questionImage: nil, answers: ["H - ANSWER1", "H - ANSWER2", "H - ANSWER3", "H - ANSWER4"], correctAnswer: "H - ANSWER1"), question(questionID: 9, questionString: "I - QUESTION?", questionImage: nil, answers: ["I - ANSWER1", "I - ANSWER2", "I - ANSWER3", "I - ANSWER4"], correctAnswer: "I - ANSWER1"), question(questionID: 10, questionString: "J - QUESTION?", questionImage: nil, answers: ["J - ANSWER1", "J - ANSWER2", "J - ANSWER3", "J - ANSWER4"], correctAnswer: "J - ANSWER1"), question(questionID: 11, questionString: "K - QUESTION?", questionImage: nil, answers: ["K - ANSWER1", "K - ANSWER2", "K - ANSWER3", "K - ANSWER4"], correctAnswer: "K - ANSWER1"), question(questionID: 12, questionString: "L - QUESTION?", questionImage: nil, answers: ["L - ANSWER1", "L - ANSWER2", "L - ANSWER3", "L - ANSWER4"], correctAnswer: "L - ANSWER1")]
+
+
+
+struct question {
     let questionID: Int
     let questionString: String
     let questionImage: UIImage?
     let answers: [String]
     let correctAnswer: String
+    var isQuestionUsed: used
+    
+    enum used {
+        case used
+        case notUsed
+    }
     
     init(questionID: Int, questionString: String, questionImage: UIImage?, answers: [String], correctAnswer: String) {
         self.questionID = questionID
@@ -22,8 +34,6 @@ class question {
         self.questionImage = questionImage
         self.answers = answers
         self.correctAnswer = correctAnswer
+        self.isQuestionUsed = .notUsed
     }
-    
 }
-
-let questionArray = [question(questionID: 1, questionString: "Question?", questionImage: nil, answers: ["Answer1", "Answer2", "Answer3", "Answer4"], correctAnswer: "Answer3"), question(questionID: 2, questionString: "QUESTION?", questionImage: nil, answers: ["ANSWER1", "ANSWER2", "ANSWER3", "ANSWER4"], correctAnswer: "ANSWER1")]
